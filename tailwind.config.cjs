6f8e6d955b9a2698a74c68460e9f170628a9a53a
cristhianlgl <cristhianlgl@hotmail.com>
2023-09-22 18:03:33 -0500

    Agregar imagenes y componente Nav

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,16 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'bright-red': "hsl(12, 88%, 59%)",
+				'dark-blue': "hsl(228, 39%, 23%)",
+				'dark-grayish-blue': 'hsl(227, 12%, 61%)',
+				'very-dark-blue': 'hsl(233, 12%, 13%)',
+				'very-pale-red': 'hsl(13, 100%, 96%)',
+				'very-light-gray': 'hsl(0, 0%, 98%)'
+			}
+		},
 	},
 	plugins: [],
 }
